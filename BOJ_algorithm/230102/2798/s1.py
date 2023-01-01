d51389cde5956f57ac1561e02a323bf95db8b46a
MoonysKR <dytjd297@gmail.com>
2023-01-02 08:39:31 +0900

    230102 [ALGO] : Start 2798

---
+++ BOJ_algorithm/230102/2798/s1.py
@@ -0,0 +1,20 @@
+import sys 
+
+sys.stdin = open('input1.txt')
+
+N , M = map(int, input().split())
+
+nums = list(map(int, input().split()))
+
+# print(nums)
+# [5, 6, 7, 8, 9]
+
+near = 0
+
+def blackjack(cnt, num, total):
+    global near
+
+    if cnt == 3:
+
+        
+    else:
