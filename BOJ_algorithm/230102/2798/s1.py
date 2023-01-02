332f41ea65573d1f20cb0e732c333cc036febb66
MoonysKR <dytjd297@gmail.com>
2023-01-03 08:28:09 +0900

    230103 [ALGO] : Solved 2798

--- BOJ_algorithm/230102/2798/s1.py
+++ BOJ_algorithm/230102/2798/s1.py
@@ -1,6 +1,46 @@
 import sys 
 
-sys.stdin = open('input1.txt')
+sys.stdin = open('input2.txt')
+
+def blackjack(lst):
+    global near, nums, N, M
+
+    if len(lst) == 3:
+        sums = 0
+        for i in range(len(lst)):
+            sums += lst[i]
+        if abs(M - near) > abs(M - sums):
+            near = sums
+
+    elif len(lst) == 0:
+        for i in range(N-2):
+            lst.append(nums[i])
+            blackjack(lst)
+            lst.pop()
+
+    elif len(lst) == 1:
+        for i in range(N-2):
+            if nums[i] == lst[0]:
+                tmp = i
+        for j in range(tmp + 1, N - 1):
+            lst.append(nums[j])
+            if sum(lst) > M:
+                lst.pop()
+            else:
+                blackjack(lst)
+                lst.pop()
+
+    elif len(lst) == 2:
+        for i in range(N-1):
+            if nums[i] == lst[1]:
+                tmp = i
+        for j in range(tmp + 1, N):
+            lst.append(nums[j])
+            if sum(lst) > M:
+                lst.pop()
+            else:
+                blackjack(lst)
+                lst.pop()
 
 N , M = map(int, input().split())
 
@@ -11,10 +51,6 @@ nums = list(map(int, input().split()))
 
 near = 0
 
-def blackjack(cnt, num, total):
-    global near
+blackjack([])
 
-    if cnt == 3:
-
-        
-    else:
+print(near)
