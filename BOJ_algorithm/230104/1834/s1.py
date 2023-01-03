02bfb8d97e5cf450e048df9dfd02bebb6960a6b7
MoonysKR <dytjd297@gmail.com>
2023-01-04 08:10:23 +0900

    230104 [ALGO] : Solved 1834

---
+++ BOJ_algorithm/230104/1834/s1.py
@@ -0,0 +1,15 @@
+import sys
+
+sys.stdin = open('input2.txt')
+
+# N으로 나누었을 때 나머지와 몫이 같은 모든 자연수의 합을 구하는 프로그램을 작성하시오. 예를 들어 N=3일 때, 나머지와 몫이 모두 같은 자연수는 4와 8 두 개가 있으므로, 그 합은 12이다.
+
+N = int(input())
+
+result = 0
+
+for i in range(1, N):
+    tmp = i + (i * N)
+    result += tmp
+
+print(result)
