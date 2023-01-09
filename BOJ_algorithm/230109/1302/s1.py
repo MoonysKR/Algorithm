c705a887b562a59db754248e76c1ff2b5e2b37f7
MoonysKR <dytjd297@gmail.com>
2023-01-10 08:29:32 +0900

    230110 [ALGO] : Solved 1302

--- BOJ_algorithm/230109/1302/s1.py
+++ BOJ_algorithm/230109/1302/s1.py
@@ -1,6 +1,6 @@
 import sys
 
-sys.stdin = open('input1.txt')
+sys.stdin = open('input5.txt')
 
 N = int(input())
 
@@ -13,13 +13,21 @@ for i in range(N):
     else:
         records[book] = 1
 
-best_seller = 0
+# print(records)
+# {'top': 4, 'kimtop': 1}
+
+new_records = sorted(records.items())
+
+# print(new_records)
+# [('kimtop', 1), ('top', 4)]
+
+best_sells = 0
+best_seller = ''
 
 
-for i in range(len(records.values())):
-    if records.values()[i] > best_seller:
-        best_seller = records.values()[i]
+for i in range(len(new_records)):
+    if new_records[i][1] > best_sells:
+        best_sells = new_records[i][1]
+        best_seller = new_records[i][0]
 
 print(best_seller)
-
-print(records)
