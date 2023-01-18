c8c53779dd5d827fcd99bb228e38a1cc6966e52b
MoonysKR <dytjd297@gmail.com>
2023-01-18 17:48:14 +0900

    Tmp 1260

--- BOJ_algorithm/230114/1260/s1.py
+++ BOJ_algorithm/230114/1260/s1.py
@@ -1,58 +1,63 @@
 import sys
 
-sys.stdin = open('input2.txt')
+sys.stdin = open('input5.txt')
 
 from collections import deque
 
-N, M, V = map(int, input().split())
+def dfs(start):
+    global info, dfs_visited
 
-info = [[] for _ in range(N)]
+    if start + 1 in dfs_visited or len(dfs_visited) == N:
+        return
+    else:
+        dfs_visited.append(start + 1)
+        for i in range(len(info[start])):
+            if info[start][i] == 1:
+                dfs(i)
 
-for i in range(M):
-    a, b = map(int, input().split())
-    info[a-1].append(b-1)
-
-for i in range(N):
-    info[i] = sorted(info[i])
-
-print(info)
-# [[1, 2, 3], [3], [3], []]
-
-visited_bfs = []
-visited_dfs = []
 
 def bfs(info):
-    global visited_bfs
+    global bfs_visited
 
-    que = deque()
+    togo = deque()
 
     for i in range(len(info[V-1])):
-        que.append((V-1, info[V-1][i]))
+        if info[V-1][i] == 1:
+            togo.append((V-1, i))
 
-    # print(que)
+    # print(togo)
     # deque([(2, 0), (2, 3)])
 
-    while que and len(visited_bfs) < N:
-        print(que)
-        start, end = que.popleft()
-        print(start, end)
-        # 출발점이 방문한 지점에 없다면(처음 시작을 위한 로직) 방문 기록에 추가
-        if start + 1 not in visited_bfs:
-            visited_bfs.append(start + 1)
-        
-        # 출발점과 연결된 지점들을 순회하여 
-        for i in range(len(info[start])):
-            # 해당 지점이 방문 기록에 없다면, 방문 기록에 추가
-            if info[start][i] + 1 not in visited_bfs:
-                visited_bfs.append(info[start][i] + 1)
-                # 방문점을 시작점으로 한 도착점 큐에 추가 
-        for j in range(len(info[end])):
-            print(len(info[end]))
-            que.append((end, info[end][j]))
-            print(end, info[end][j])
-    
-    # print(visited_bfs)
-    
-bfs(info)
+    while togo and len(bfs_visited) != N:
+        # print(togo)
 
-print(visited_bfs)
+        start, end = togo.popleft()
+
+        if len(bfs_visited) == 0:
+            bfs_visited.append(start + 1)
+        
+        if end + 1 not in bfs_visited:
+            bfs_visited.append(end + 1)
+            for i in range(len(info[end])):
+                if info[end][i] == 1: 
+                    togo.append((end, i))
+
+
+N, M, V = map(int, input().split())
+
+info = [[0]* N for _ in range(N)]
+
+for i in range(M):
+    start, end = map(int, input().split())
+    info[start-1][end-1] = 1
+    info[end-1][start-1] = 1
+
+# print(*info, sep='\n')
+
+bfs_visited = []
+dfs_visited = []
+
+dfs(V-1)
+print(*dfs_visited)
+bfs(info)
+print(*bfs_visited)
