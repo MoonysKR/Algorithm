e3662d70fc6c4df4daaac83e0571405e6ad7ea02
MoonysKR <dytjd297@gmail.com>
2023-01-22 20:20:13 +0900

    230122 [ALGO] : Solved 1080

--- BOJ_algorithm/230121/1080/s1.py
+++ BOJ_algorithm/230121/1080/s1.py
@@ -1,6 +1,6 @@
 import sys
 
-sys.stdin = open('input4.txt')
+sys.stdin = open('input5.txt')
 
 N, M = map(int, input().split())
 
@@ -26,24 +26,23 @@ for i in range(N):
 cnt = 0
 
 if N < 3 or M < 3:
-    cnt = -1
-elif lst_A == lst_B:
-    pass
+    if lst_A != lst_B:
+        cnt = -1
+    else:
+        cnt = 0
 else:
     for i in range(N-2):
         for j in range(M-2):
 
-            for k in range(3):
-                for l in range(3):
-                    if lst_A[i + k][j + l] == 0:
-                        lst_A[i + k][j + l] = 1
-                    else:
-                        lst_A[i + k][j + l] = 0
-            cnt += 1
-            print('A', cnt)
-            print(*lst_A, sep='\n')
-            print('B', cnt)
-            print(*lst_B, sep='\n')
+            if lst_A[i][j] != lst_B[i][j]:
+                for k in range(3):
+                    for l in range(3):
+                        if lst_A[i + k][j + l] == 0:
+                            lst_A[i + k][j + l] = 1
+                        else:
+                            lst_A[i + k][j + l] = 0
+                cnt += 1
+
             if lst_A == lst_B:
                 break
 
